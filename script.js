1d8596ea08c36746adcdc534e92d861b36bd6b00
Ivan Torcuato <2137332@slu.edu.ph>
2018-03-17 17:28:48 +0800

    adding of map in index

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+var map;
+function initMap() {
+	map = new google.maps.Map(document.getElementById("map"), {
+	  center: {lat: -34.397, lng: 150.644},
+	  zoom: 8
+	});
+}
+
 function dropdownMenu() {
   var dropdown = document.getElementById("dropdownClick");
   if(dropdown.className === "topnav") {

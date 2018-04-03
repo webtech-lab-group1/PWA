2477648ffa9cbcd1a78b0a664443165a8a5f7e7d
Ivan Torcuato <2137332@slu.edu.ph>
2018-04-03 22:31:00 +0800

    some changes

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+var area = document.getElementById('map');
+var context = area.getContext('2d');
+
+make_base();
+
+function make_base() {
+	var base_image = new Image();
+	base_image.src = 'map/zoom15.bmp';
+	base_image.onload = function() {
+		var imgwid = base_image.width;
+		var imghgt = base_image.height;
+		context.canvas.width = imgwid;
+		context.canvas.height = imghgt;
+		context.drawImage(base_image, 0, 0, imgwid, imghgt);
+	}
+}
+
 function dropdownMenu() {
   var dropdown = document.getElementById("dropdownClick");
   if(dropdown.className === "topnav") {

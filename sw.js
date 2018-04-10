49b02a9c418d4e479b823a0396b36ba58e22c8fe
Ivan Torcuato <2137332@slu.edu.ph>
2018-04-10 10:14:29 +0800

    changes

--- sw.js
+++ sw.js
@@ -1,24 +1,4 @@
-self.addEventListener('install', e => {
-  const timeStamp = Date.now();
-  e.waitUntil(
-    caches.open('RestoMapp').then(cache => {
-      return cache.addAll([
-        `/`,
-        `/index.html?timestamp=${timeStamp}`,
-        `/style.css?timestamp=${timeStamp}`,
-        `/script.js?timestamp=${timeStamp}`,
-        `/map/zoom15.bmp?timestamp=${timeStamp}`,
-        `/dragscroll.js?timestamp=${timeStamp}`,
-        `/Content/Restaurant.json?timestamp=${timeStamp}`,
-        `/manifest.json?timestamp=${timeStamp}`,
-        `images/icons/icon-144x144.png?timestamp=${timeStamp}`
-      ])
-          .then(() => self.skipWaiting());
-    })
-  );
-});
-
-let CACHE = 'cache';
+let CACHE = `RestoMapp`;
 
 self.addEventListener('install', function(evt) {
     console.log('The service worker is being installed.');
